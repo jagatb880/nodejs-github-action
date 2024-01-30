a8c3f075f8a1e4d392b57a1285420761c258398a
jagat.sahoo <jagat.sahoo@robosoftin.com>
2024-01-30 12:03:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+
+const app = express();
+const port = 3000;
+
+// Set your AWS region
+AWS.config.update({ region: 'us-east-1' });
+
+// Function to fetch secrets from AWS Secrets Manager
+async function getSecrets(secretName) {
+    const secretsManager = new AWS.SecretsManager();
+
+    try {
+        const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+        const secretString = data.SecretString;
+
+        return JSON.parse(secretString);
+    } catch (error) {
+        console.error(`Error fetching secret "${secretName}":`, error);
+        throw error;
+    }
+}
+
+// Example usage in an asynchronous function
+async function main() {
+    const secretName = 'my-secret';
+
+    try {
+        const secrets = await getSecrets(secretName);
+        console.log('Secrets:', secrets);
+
+        // Use the secrets in your application logic
+        // For example:
+        console.log('Key1:', secrets.db_name);
+        console.log('Key2:', secrets.username);
+        console.log('Key3:', secrets.password);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+// Run the main function
+main();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, World!</h1>');
+});
+
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
