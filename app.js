eaea33a623675675ca8ac709b10b4c23b22755d4
jagat.sahoo <jagat.sahoo@robosoftin.com>
2024-01-30 17:01:15 +0530

    port 5000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const AWS = require('aws-sdk');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Set your AWS region
 AWS.config.update({ region: 'us-east-1' });
