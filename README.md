d3c664036b60a950db7eb9836c36953da400d52a
jagat.sahoo <jagat.sahoo@robosoftin.com>
2024-02-01 17:35:44 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Reference blog t use terraform as IaC
+https://medium.com/@vladkens/aws-ecs-cluster-on-ec2-with-terraform-2023-fdb9f6b7db07
+
 ![Local Image](infra_creation_ecs.png)
---
+++ terraform/main.tf
@@ -0,0 +1,440 @@
+provider "aws" {
+  region = "us-east-1" # Change to your desired AWS region
+}
+
+# --- VPC ---
+
+data "aws_availability_zones" "available" { state = "available" }
+
+locals {
+  azs_count = 2
+  azs_names = data.aws_availability_zones.available.names
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = { Name = "demo-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count                   = local.azs_count
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = local.azs_names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
+  map_public_ip_on_launch = true
+  tags                    = { Name = "demo-public-${local.azs_names[count.index]}" }
+}
+
+# --- Internet Gateway ---
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "demo-igw" }
+}
+
+resource "aws_eip" "main" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.main]
+  tags       = { Name = "demo-eip-${local.azs_names[count.index]}" }
+}
+
+# --- Public Route Table ---
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "demo-rt-public" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Create an ECS cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "nodejs-app-cluster"
+}
+
+# --- ECS Node Role ---
+
+data "aws_iam_policy_document" "ecs_node_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_node_role" {
+  name_prefix        = "demo-ecs-node-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_node_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_node_role_policy" {
+  role       = aws_iam_role.ecs_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_node" {
+  name_prefix = "demo-ecs-node-profile"
+  path        = "/ecs/instance/"
+  role        = aws_iam_role.ecs_node_role.name
+}
+
+# --- ECS Node SG ---
+
+resource "aws_security_group" "ecs_node_sg" {
+  name_prefix = "demo-ecs-node-sg-"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- ECS Launch Template ---
+
+data "aws_ssm_parameter" "ecs_node_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "ecs_ec2" {
+  name_prefix            = "demo-ecs-ec2-"
+  image_id               = data.aws_ssm_parameter.ecs_node_ami.value
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
+
+  iam_instance_profile { arn = aws_iam_instance_profile.ecs_node.arn }
+  monitoring { enabled = true }
+
+  user_data = base64encode(<<-EOF
+      #!/bin/bash
+      echo ECS_CLUSTER=${aws_ecs_cluster.my_cluster.name} >> /etc/ecs/ecs.config;
+    EOF
+  )
+}
+
+# --- ECS ASG ---
+
+resource "aws_autoscaling_group" "ecs" {
+  name_prefix               = "demo-ecs-asg-"
+  vpc_zone_identifier       = aws_subnet.public[*].id
+  min_size                  = 2
+  max_size                  = 8
+  health_check_grace_period = 0
+  health_check_type         = "EC2"
+  protect_from_scale_in     = false
+
+  launch_template {
+    id      = aws_launch_template.ecs_ec2.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "demo-ecs-cluster"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = ""
+    propagate_at_launch = true
+  }
+}
+
+# --- ECS Capacity Provider ---
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "demo-ecs-ec2"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.my_cluster.name
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+}
+
+# Create an ECR repository for your Node.js app
+data "aws_ecr_repository" "existing_repository" {
+  name = "nodejs-app"
+}
+
+output "demo_app_repo_url" {
+  value = data.aws_ecr_repository.existing_repository.repository_url
+}
+
+# --- ECS Task Role ---
+
+data "aws_iam_policy_document" "ecs_task_doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name_prefix        = "demo-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role" "ecs_exec_role" {
+  name_prefix        = "demo-ecs-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
+  role       = aws_iam_role.ecs_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# --- Cloud Watch Logs ---
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/demo"
+  retention_in_days = 14
+}
+
+# --- ECS Task Definition ---
+
+resource "aws_ecs_task_definition" "app" {
+  family             = "demo-app"
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  execution_role_arn = aws_iam_role.ecs_exec_role.arn
+  network_mode       = "awsvpc"
+  cpu                = 256
+  memory             = 512
+
+  container_definitions = jsonencode([{
+    name         = "app",
+    image        = "${data.aws_ecr_repository.existing_repository.repository_url}:latest",
+    essential    = true,
+    portMappings = [{ containerPort = 5000, hostPort = 5000 }],
+
+    environment = [
+      { name = "DEV", value = "dev" }
+    ]
+
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        "awslogs-region"        = "us-east-1",
+        "awslogs-group"         = aws_cloudwatch_log_group.ecs.name,
+        "awslogs-stream-prefix" = "app"
+      }
+    },
+  }])
+}
+
+# --- ECS Service ---
+
+resource "aws_security_group" "ecs_task" {
+  name_prefix = "ecs-task-sg-"
+  description = "Allow all traffic within the VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "app"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 2
+
+  network_configuration {
+    security_groups = [aws_security_group.ecs_task.id]
+    subnets         = aws_subnet.public[*].id
+  }
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+
+  ordered_placement_strategy {
+    type  = "spread"
+    field = "attribute:ecs.availability-zone"
+  }
+
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = "app"
+    container_port   = 5000
+  }
+}
+
+# --- ALB ---
+
+resource "aws_security_group" "http" {
+  name_prefix = "http-sg-"
+  description = "Allow all HTTP/HTTPS traffic from public"
+  vpc_id      = aws_vpc.main.id
+
+  dynamic "ingress" {
+    for_each = [80, 443]
+    content {
+      protocol    = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "demo-alb"
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id
+  security_groups    = [aws_security_group.http.id]
+}
+
+resource "aws_lb_target_group" "app" {
+  name_prefix = "app-"
+  vpc_id      = aws_vpc.main.id
+  protocol    = "HTTP"
+  port        = 80
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    path                = "/"
+    port                = 80
+    matcher             = 200
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.id
+  }
+}
+
+output "alb_url" {
+  value = aws_lb.main.dns_name
+}
+
+# # Create a task definition for your Node.js app
+# resource "aws_ecs_task_definition" "my_task_definition" {
+#   family                   = "my-nodejs-app"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["EC2"]
+#   cpu                      = 256
+#   container_definitions = jsonencode([{
+#     name      = "nodejs-app-container"
+#     image     = "708287751177.dkr.ecr.us-east-1.amazonaws.com/nodejs-app:latest"
+#     cpu       = 256
+#     memory    = 512
+#     essential = true
+#     portMappings = [{
+#       containerPort = 5000
+#       hostPort      = 5000
+#       protocol      = "tcp"
+#     }]
+
+#     log_configuration = {
+#       log_driver = "awslogs"
+#       options = {
+#         "awslogs-group"         = "/ecs/my-nodejs-app"
+#         "awslogs-region"        = "us-east-1"
+#         "awslogs-stream-prefix" = "nodejs-app-container"
+#       }
+#     }
+#   }])
+# }
+
+# # Create an ECS service for your Node.js app
+# resource "aws_ecs_service" "my_service" {
+#   name            = "nodejs-app-service"
+#   cluster         = aws_ecs_cluster.my_cluster.id
+#   task_definition = aws_ecs_task_definition.my_task_definition.arn
+#   # Specify the desired count of tasks
+#   desired_count        = 1
+#   force_new_deployment = true
+#   launch_type          = "EC2"
+
+#   network_configuration {
+#     subnets         = ["subnet-07b702d8f3598c8d7", "subnet-0fc0856237e2e968a", "subnet-03db0ba02ce67617f"] # Replace with your subnet IDs
+#     security_groups = ["sg-0260b94959bc2f546"]                                                             # Replace with your security group IDs
+#   }
+
+
+#   depends_on = [aws_ecs_task_definition.my_task_definition]
+# }
