92103647d27cb60740ceb53a390e2ce4871aa8a8
jagat.sahoo <jagat.sahoo@robosoftin.com>
2024-02-02 15:45:11 +0530

    updated from demo to ngapro

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,36 +15,37 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags                 = { Name = "demo-vpc" }
+  tags                 = { Name = "ngapro-vpc" }
 }
 
 resource "aws_subnet" "public" {
-  count                   = local.azs_count
-  vpc_id                  = aws_vpc.main.id
-  availability_zone       = local.azs_names[count.index]
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
+  count             = local.azs_count
+  vpc_id            = aws_vpc.main.id
+  availability_zone = local.azs_names[count.index]
+  #cidrsubnet(ip_block, newbits, netnum)
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index) # 10.10.11.0/24, 10.10.12.0/24
   map_public_ip_on_launch = true
-  tags                    = { Name = "demo-public-${local.azs_names[count.index]}" }
+  tags                    = { Name = "ngapro-public-${local.azs_names[count.index]}" }
 }
 
 # --- Internet Gateway ---
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  tags   = { Name = "demo-igw" }
+  tags   = { Name = "ngapro-igw" }
 }
 
 resource "aws_eip" "main" {
   count      = local.azs_count
   depends_on = [aws_internet_gateway.main]
-  tags       = { Name = "demo-eip-${local.azs_names[count.index]}" }
+  tags       = { Name = "ngapro-eip-${local.azs_names[count.index]}" }
 }
 
 # --- Public Route Table ---
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  tags   = { Name = "demo-rt-public" }
+  tags   = { Name = "ngapro-rt-public" }
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -78,7 +79,7 @@ data "aws_iam_policy_document" "ecs_node_doc" {
 }
 
 resource "aws_iam_role" "ecs_node_role" {
-  name_prefix        = "demo-ecs-node-role"
+  name_prefix        = "ngapro-ecs-node-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_node_doc.json
 }
 
@@ -88,7 +89,7 @@ resource "aws_iam_role_policy_attachment" "ecs_node_role_policy" {
 }
 
 resource "aws_iam_instance_profile" "ecs_node" {
-  name_prefix = "demo-ecs-node-profile"
+  name_prefix = "ngapro-ecs-node-profile"
   path        = "/ecs/instance/"
   role        = aws_iam_role.ecs_node_role.name
 }
@@ -96,7 +97,7 @@ resource "aws_iam_instance_profile" "ecs_node" {
 # --- ECS Node SG ---
 
 resource "aws_security_group" "ecs_node_sg" {
-  name_prefix = "demo-ecs-node-sg-"
+  name_prefix = "ngapro-ecs-node-sg-"
   vpc_id      = aws_vpc.main.id
 
   egress {
@@ -114,7 +115,7 @@ data "aws_ssm_parameter" "ecs_node_ami" {
 }
 
 resource "aws_launch_template" "ecs_ec2" {
-  name_prefix            = "demo-ecs-ec2-"
+  name_prefix            = "ngapro-ecs-ec2-"
   image_id               = data.aws_ssm_parameter.ecs_node_ami.value
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
@@ -132,7 +133,7 @@ resource "aws_launch_template" "ecs_ec2" {
 # --- ECS ASG ---
 
 resource "aws_autoscaling_group" "ecs" {
-  name_prefix               = "demo-ecs-asg-"
+  name_prefix               = "ngapro-ecs-asg-"
   vpc_zone_identifier       = aws_subnet.public[*].id
   min_size                  = 2
   max_size                  = 8
@@ -147,7 +148,7 @@ resource "aws_autoscaling_group" "ecs" {
 
   tag {
     key                 = "Name"
-    value               = "demo-ecs-cluster"
+    value               = "ngapro-ecs-cluster"
     propagate_at_launch = true
   }
 
@@ -161,7 +162,7 @@ resource "aws_autoscaling_group" "ecs" {
 # --- ECS Capacity Provider ---
 
 resource "aws_ecs_capacity_provider" "main" {
-  name = "demo-ecs-ec2"
+  name = "ngapro-ecs-ec2"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
@@ -192,7 +193,7 @@ data "aws_ecr_repository" "existing_repository" {
   name = "nodejs-app"
 }
 
-output "demo_app_repo_url" {
+output "ngapro_app_repo_url" {
   value = data.aws_ecr_repository.existing_repository.repository_url
 }
 
@@ -211,12 +212,12 @@ data "aws_iam_policy_document" "ecs_task_doc" {
 }
 
 resource "aws_iam_role" "ecs_task_role" {
-  name_prefix        = "demo-ecs-task-role"
+  name_prefix        = "ngapro-ecs-task-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
 }
 
 resource "aws_iam_role" "ecs_exec_role" {
-  name_prefix        = "demo-ecs-exec-role"
+  name_prefix        = "ngapro-ecs-exec-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_doc.json
 }
 
@@ -228,14 +229,14 @@ resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
 # --- Cloud Watch Logs ---
 
 resource "aws_cloudwatch_log_group" "ecs" {
-  name              = "/ecs/demo"
+  name              = "/ecs/ngapro"
   retention_in_days = 14
 }
 
 # --- ECS Task Definition ---
 
 resource "aws_ecs_task_definition" "app" {
-  family             = "demo-app"
+  family             = "ngapro-app"
   task_role_arn      = aws_iam_role.ecs_task_role.arn
   execution_role_arn = aws_iam_role.ecs_exec_role.arn
   network_mode       = "awsvpc"
@@ -351,7 +352,7 @@ resource "aws_security_group" "http" {
 }
 
 resource "aws_lb" "main" {
-  name               = "demo-alb"
+  name               = "ngapro-alb"
   load_balancer_type = "application"
   subnets            = aws_subnet.public[*].id
   security_groups    = [aws_security_group.http.id]
